5c88ab39c416a31ccafb10c2203552b6a2a9a15b
Andris Raugulis <moo@arthepsy.eu>
2017-02-23 14:35:57 +0200

    Update year. Use scala.meta dependency.

--- src/test/java/eu/arthepsy/sonar/plugins/scala/ScalaPluginTest.java
+++ src/test/java/eu/arthepsy/sonar/plugins/scala/ScalaPluginTest.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2016 Andris Raugulis
+ * Copyright (c) 2017 Andris Raugulis
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
